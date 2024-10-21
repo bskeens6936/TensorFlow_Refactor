368e867c800d93d3ac23cb3e16bd0fbe23419b2d
bskeens6936 <bskeens6936@sdsu.edu>
2024-10-21 00:09:45 -0700

    Refactor commits. Changed training.py, added utesting in test_refactor.py

---
+++ CS635_Utesting/test_refactor.py
@@ -0,0 +1,54 @@
+'''
+    Brandon Skeens
+    826416091
+    To run unit tests, run:
+        python3 -m unittest test_refactor.py 
+        (not sure if this changes between machines, I have python3 on mine so the cmd might be slightly different)
+    Will test the added sub functions to ensure the behavior is expected
+'''
+
+import unittest
+# Importing individually, not sure why it was freaking out but
+# this was the solution I found
+import tensorflow.python.keras as tfKeras
+import tensorflow.python.keras.layers as tfLayers
+import tensorflow
+
+# Import where refactor was done
+from tensorflow.python.keras.engine import training
+
+# Define unit tests
+class TestYourModelClass(unittest.TestCase):
+    def test_default_initialization(self):
+        model = training.Model()
+        self.assertIsNone(model.compiled_loss)
+        self.assertIsNone(model.compiled_metrics)
+
+    def test_subclassed_functional_model_initialization(self):
+        inputs = tfKeras.Input(shape=(32,))
+        outputs = tfLayers.Dense(1)(inputs)
+        model = training.Model(inputs=inputs, outputs=outputs)
+        self.assertTrue(model._is_model_for_instrumentation)
+        self.assertIsNotNone(model.inputs)
+        self.assertIsNotNone(model.outputs)
+
+    def test_invalid_keyword_arguments(self):
+        with self.assertRaises(TypeError):
+            training.Model(invalid_param='value')
+
+    def test_attribute_initialization(self):
+        model = training.Model()
+        self.assertEqual(model.stop_training, False)
+        self.assertIsNone(model.history)
+     
+    def test_model_state_after_initialization(self):
+        model = training.Model()
+        self.assertFalse(model._is_graph_network)
+
+    def test_base_model_initialized(self):
+        model = training.Model()
+        self.assertTrue(model._base_model_initialized)
+
+# Run unit tests
+if __name__ == '__main__':
+    unittest.main()
--- tensorflow/python/keras/engine/training.py
+++ tensorflow/python/keras/engine/training.py
@@ -218,101 +218,235 @@ class Model(base_layer.Layer, version_utils.ModelVersionSelector):
     else:
       return super(Model, cls).__new__(cls, *args, **kwargs)
 
+  '''
+  CS 635 ORIGINAL FUNC COMMENTED OUT FOR REFERENCE
+  '''
+
+  # @trackable.no_automatic_dependency_tracking
+  # def __init__(self, *args, **kwargs):
+  #   self._is_model_for_instrumentation = True
+
+  #   # Special case for Subclassed Functional Model, which we couldn't detect
+  #   # when __new__ is called. We only realize it is a functional model when it
+  #   # calls super.__init__ with input and output tensor.
+  #   from tensorflow.python.keras.engine import functional  # pylint: disable=g-import-not-at-top
+  #   if (is_functional_model_init_params(args, kwargs) and
+  #       not isinstance(self, functional.Functional)):
+  #     # Filter the kwargs for multiple inheritance.
+  #     supported_kwargs = ['inputs', 'outputs', 'name', 'trainable', 'skip_init']
+  #     model_kwargs = {k: kwargs[k] for k in kwargs if k in supported_kwargs}
+  #     other_kwargs = {k: kwargs[k] for k in kwargs if k not in supported_kwargs}
+  #     inject_functional_model_class(self.__class__)
+  #     functional.Functional.__init__(self, *args, **model_kwargs)
+
+  #     # In case there is any multiple inheritance here, we need to call the
+  #     # __init__ for any class that appears after the Functional class.
+  #     clz_to_init = []
+  #     found_functional_class = False
+  #     for clz in self.__class__.__bases__:
+  #       if issubclass(clz, functional.Functional):
+  #         found_functional_class = True
+  #         continue
+  #       if found_functional_class:
+  #         clz_to_init.append(clz)
+
+  #     if clz_to_init:
+  #       for clz in clz_to_init:
+  #         clz.__init__(self, *args, **other_kwargs)
+  #     elif other_kwargs:
+  #       # In case there are unused kwargs, we should raise an error to user, in
+  #       # case they have a typo in the param name.
+  #       raise TypeError(
+  #           'The following keyword arguments aren\'t supported: {}'.format(
+  #               other_kwargs))
+  #     return
+
+  #   # The following are implemented as property functions:
+  #   # self.trainable_weights
+  #   # self.non_trainable_weights
+  #   # `inputs` / `outputs` will only appear in kwargs if either are misspelled.
+  #   generic_utils.validate_kwargs(kwargs, {
+  #       'trainable', 'dtype', 'dynamic', 'name', 'autocast', 'inputs', 'outputs'
+  #   })
+  #   super(Model, self).__init__(**kwargs)
+  #   # By default, Model is a subclass model, which is not in graph network.
+  #   self._is_graph_network = False
+
+  #   self.inputs = None
+  #   self.outputs = None
+  #   self.input_names = None
+  #   self.output_names = None
+  #   # stop_training is used by callback to stop training when error happens
+  #   self.stop_training = False
+  #   self.history = None
+  #   # These objects are used in the default `Model.compile`. They are not
+  #   # guaranteed to be set after `Model.compile` is called, as users can
+  #   # override compile with custom logic.
+  #   self.compiled_loss = None
+  #   self.compiled_metrics = None
+
+  #   # This is True for Sequential networks and Functional networks.
+  #   self._compute_output_and_mask_jointly = False
+
+  #   # Don't reset compilation if already done. This may occur if calling
+  #   # `__init__` (or `_init_graph_network`) on an already-compiled model
+  #   # such as a Sequential model. Sequential models may need to rebuild
+  #   # themselves after compilation.
+  #   self._maybe_create_attribute('_is_compiled', False)
+  #   self._maybe_create_attribute('optimizer', None)
+
+  #   # Model must be created under scope of DistStrat it will be trained with.
+  #   if distribute_lib.has_strategy():
+  #     self._distribution_strategy = distribute_lib.get_strategy()
+  #   else:
+  #     self._distribution_strategy = None
+
+  #   self._cluster_coordinator = None
+
+  #   # Defaults to value of `tf.config.experimental_functions_run_eagerly`.
+  #   self._run_eagerly = None
+  #   # Initialize cache attrs.
+  #   self._reset_compile_cache()
+
+  #   # Fault-tolerance handler. Set in `ModelCheckpoint`.
+  #   self._training_state = None
+  #   self._saved_model_inputs_spec = None
+  #   self._checkpoint = trackable_utils.Checkpoint(root=weakref.ref(self))
+
+  #   self._steps_per_execution = None
+
+  #   self._init_batch_counters()
+  #   self._base_model_initialized = True
+    
+  '''
+  CS635 Assignment Refactoring:
+    Refactored init function. Started out long and complicated,
+    split into multiple helper functions to help with simplification
+    and readability
+  '''
   @trackable.no_automatic_dependency_tracking
   def __init__(self, *args, **kwargs):
-    self._is_model_for_instrumentation = True
+      self._is_model_for_instrumentation = True
 
-    # Special case for Subclassed Functional Model, which we couldn't detect
-    # when __new__ is called. We only realize it is a functional model when it
-    # calls super.__init__ with input and output tensor.
-    from tensorflow.python.keras.engine import functional  # pylint: disable=g-import-not-at-top
-    if (is_functional_model_init_params(args, kwargs) and
-        not isinstance(self, functional.Functional)):
-      # Filter the kwargs for multiple inheritance.
+      # Check if the model is a subclassed functional model
+      if self._is_subclassed_functional_model(args, kwargs):
+          self._handle_functional_model_initialization(args, kwargs)
+          return
+
+      # Validate keyword args, init model w/ helper methods
+      self._validate_kwargs(kwargs)
+      self._initialize_attributes(kwargs)
+      self._initialize_compile_settings()
+
+  @trackable.no_automatic_dependency_tracking
+  def _is_subclassed_functional_model(self, args, kwargs):
+      """
+      Helper function to check if the model is a subclassed functional model
+      """
+      from tensorflow.python.keras.engine import functional  # pylint: disable=g-import-not-at-top
+
+      # Check if the model is initialized with functional model params
+      return (is_functional_model_init_params(args, kwargs) and
+              not isinstance(self, functional.Functional))
+
+  @trackable.no_automatic_dependency_tracking
+  def _handle_functional_model_initialization(self, args, kwargs):
+      """
+      Handle initialization when the model is a subclassed functional model.
+      Initialize functional model, handle any multiple inheritance
+      """
+      from tensorflow.python.keras.engine import functional  # pylint: disable=g-import-not-at-top
       supported_kwargs = ['inputs', 'outputs', 'name', 'trainable', 'skip_init']
       model_kwargs = {k: kwargs[k] for k in kwargs if k in supported_kwargs}
       other_kwargs = {k: kwargs[k] for k in kwargs if k not in supported_kwargs}
+
+      # Inject functional model class and init functional model
       inject_functional_model_class(self.__class__)
       functional.Functional.__init__(self, *args, **model_kwargs)
 
-      # In case there is any multiple inheritance here, we need to call the
-      # __init__ for any class that appears after the Functional class.
+      # Init any classes in multiple inheritance chains
+      self._init_multiple_inheritance_classes(args, other_kwargs)
+
+  @trackable.no_automatic_dependency_tracking
+  def _validate_kwargs(self, kwargs):
+      """
+      Validates provided keyword args to ensure they are supported
+      Raises error if unsupported keyword args are found
+      """
+      valid_kwargs = {'trainable', 'dtype', 'dynamic', 'name', 'autocast', 'inputs', 'outputs'}
+      generic_utils.validate_kwargs(kwargs, valid_kwargs)
+
+  @trackable.no_automatic_dependency_tracking
+  def _initialize_attributes(self, kwargs):
+      """
+      Initializes attributes related to model config and settings
+      """
+      super(Model, self).__init__(**kwargs)
+      self._is_graph_network = False
+      self.inputs = None
+      self.outputs = None
+      self.input_names = None
+      self.output_names = None
+      self.stop_training = False
+      self.history = None
+      self.compiled_loss = None
+      self.compiled_metrics = None
+      self._compute_output_and_mask_jointly = False
+      self._maybe_create_attribute('_is_compiled', False)
+      self._maybe_create_attribute('optimizer', None)
+      
+      # Init the distribution strategy (if exists)
+      if distribute_lib.has_strategy():
+          self._distribution_strategy = distribute_lib.get_strategy()
+      else:
+          self._distribution_strategy = None
+      
+      self._cluster_coordinator = None
+      self._run_eagerly = None
+      self._reset_compile_cache()
+      self._training_state = None
+      self._saved_model_inputs_spec = None
+      self._checkpoint = trackable_utils.Checkpoint(root=weakref.ref(self))
+      self._steps_per_execution = None
+      self._init_batch_counters()
+      self._base_model_initialized = True
+
+  @trackable.no_automatic_dependency_tracking
+  def _initialize_compile_settings(self):
+      """
+      Initializes settings related to model compilation to help setup for training
+      """
+      self.compiled_loss = None
+      self.compiled_metrics = None
+      self._compute_output_and_mask_jointly = False
+      self._maybe_create_attribute('_is_compiled', False)
+      self._maybe_create_attribute('optimizer', None)
+
+  @trackable.no_automatic_dependency_tracking
+  def _init_multiple_inheritance_classes(self, args, other_kwargs):
+      """
+      Initialize classes in inheritance chain after functional class
+      """
+      from tensorflow.python.keras.engine import functional  # pylint: disable=g-import-not-at-top
+
       clz_to_init = []
       found_functional_class = False
+
+      # Iterate through base classes and find those after functional
       for clz in self.__class__.__bases__:
-        if issubclass(clz, functional.Functional):
-          found_functional_class = True
-          continue
-        if found_functional_class:
-          clz_to_init.append(clz)
+          if issubclass(clz, functional.Functional):
+              found_functional_class = True
+              continue
+          if found_functional_class:
+              clz_to_init.append(clz)
 
+      # Initialize classes if found
       if clz_to_init:
-        for clz in clz_to_init:
-          clz.__init__(self, *args, **other_kwargs)
+          for clz in clz_to_init:
+              clz.__init__(self, *args, **other_kwargs)
       elif other_kwargs:
-        # In case there are unused kwargs, we should raise an error to user, in
-        # case they have a typo in the param name.
-        raise TypeError(
-            'The following keyword arguments aren\'t supported: {}'.format(
-                other_kwargs))
-      return
-
-    # The following are implemented as property functions:
-    # self.trainable_weights
-    # self.non_trainable_weights
-    # `inputs` / `outputs` will only appear in kwargs if either are misspelled.
-    generic_utils.validate_kwargs(kwargs, {
-        'trainable', 'dtype', 'dynamic', 'name', 'autocast', 'inputs', 'outputs'
-    })
-    super(Model, self).__init__(**kwargs)
-    # By default, Model is a subclass model, which is not in graph network.
-    self._is_graph_network = False
-
-    self.inputs = None
-    self.outputs = None
-    self.input_names = None
-    self.output_names = None
-    # stop_training is used by callback to stop training when error happens
-    self.stop_training = False
-    self.history = None
-    # These objects are used in the default `Model.compile`. They are not
-    # guaranteed to be set after `Model.compile` is called, as users can
-    # override compile with custom logic.
-    self.compiled_loss = None
-    self.compiled_metrics = None
-
-    # This is True for Sequential networks and Functional networks.
-    self._compute_output_and_mask_jointly = False
-
-    # Don't reset compilation if already done. This may occur if calling
-    # `__init__` (or `_init_graph_network`) on an already-compiled model
-    # such as a Sequential model. Sequential models may need to rebuild
-    # themselves after compilation.
-    self._maybe_create_attribute('_is_compiled', False)
-    self._maybe_create_attribute('optimizer', None)
-
-    # Model must be created under scope of DistStrat it will be trained with.
-    if distribute_lib.has_strategy():
-      self._distribution_strategy = distribute_lib.get_strategy()
-    else:
-      self._distribution_strategy = None
-
-    self._cluster_coordinator = None
-
-    # Defaults to value of `tf.config.experimental_functions_run_eagerly`.
-    self._run_eagerly = None
-    # Initialize cache attrs.
-    self._reset_compile_cache()
-
-    # Fault-tolerance handler. Set in `ModelCheckpoint`.
-    self._training_state = None
-    self._saved_model_inputs_spec = None
-    self._checkpoint = trackable_utils.Checkpoint(root=weakref.ref(self))
-
-    self._steps_per_execution = None
-
-    self._init_batch_counters()
-    self._base_model_initialized = True
+          # Raise error if unsupported keyword args are provided
+          raise TypeError('The following keyword arguments aren\'t supported: {}'.format(other_kwargs))
 
   @trackable.no_automatic_dependency_tracking
   def _init_batch_counters(self):
